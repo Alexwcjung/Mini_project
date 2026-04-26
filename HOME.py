c670ebdfcdc3056a3754fb72510adde80e390fa3
Alexwcjung <127401494+Alexwcjung@users.noreply.github.com>
2026-04-26 17:50:37 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -4,7 +4,9 @@ st.markdown("### ✅ 이미지 수정 테스트 중")
 st.markdown("#### Spring 2026")
 st.caption("This page is continuously updated.")
 
+IMAGE_URL = "https://raw.githubusercontent.com/Alexwcjung/Fun-English/main/a143182b-832c-4a27-87fb-74214eabb338.png?v=11"
+
 st.image(
-    "fun_english_new.png",
+    IMAGE_URL,
     use_container_width=True
 )
